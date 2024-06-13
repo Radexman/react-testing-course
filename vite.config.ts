b418b2ebc0923d7397de205e73fb1b73d43e8652
Radexman <radoslaw.siek-web.dev@wp.pl>
2024-06-13 14:18:22 +0200

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { configDefaults, defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+    forceRerunTriggers: [...configDefaults.forceRerunTriggers, "src/index.ts"],
+  },
+});
